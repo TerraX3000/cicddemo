25b36d6c40bfcd25484294549123f7e60d045302
Ken Kranz <ken.kranz57@gmail.com>
2020-12-07 16:34:46 -0500

    added whenever

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Here Today
+          Learn React Here Today or Whenever
         </a>
       </header>
     </div>
